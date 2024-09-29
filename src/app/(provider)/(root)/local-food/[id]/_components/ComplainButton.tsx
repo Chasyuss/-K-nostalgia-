5a5ac09182dabe53c95a3548773f31b55f847e16
t2023-m0081 <t2023-m0081@t2023-m0081ui-MacBookAir.local>
2024-09-29 23:12:31 +0900

    fix : 특산물 카테고리 수정

--- src/app/(provider)/(root)/local-food/[id]/_components/ComplainButton.tsx
+++ src/app/(provider)/(root)/local-food/[id]/_components/ComplainButton.tsx
@@ -7,9 +7,10 @@ export const ComplainButton = () => {
     <>
       <button
         onClick={() => setComplain(true)}
-        className="text-label-assistive font-normal text-sm"
+        className="text-label-assistive font-normal text-sm flex items-center gap-1"
       >
-        신고하기
+        <img src="/image/complaint.png" alt="신고아이콘" />
+        <p>신고하기</p>
       </button>
       {complain && <div>모달</div>}
     </>
