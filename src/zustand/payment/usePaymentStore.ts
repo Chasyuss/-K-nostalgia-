2706497e435a0ee7e9153378cac9723aeaa49c08
jognhoon <jonghoon7431@gmail.com>
2024-11-20 03:47:18 +0900

    feat: 결제 요청에 필요한 값 전역 관리(zustand)

--- src/zustand/payment/usePaymentStore.ts
+++ src/zustand/payment/usePaymentStore.ts
@@ -1,15 +1,59 @@
 import { create } from "zustand";
 
-interface ProductItem{
-  amount:number,
-  id: string,
-  name: string,
-  orderNameArr: string[]
-  quantity: number
+//결제 요청 정보에 필요한 값 전역 저장
+//update: 24.11.20
+
+type Products = {
+  id: string;
+  name: string;
+  amount: number;
+  quantity: number;
+}[]
+type Customer = {
+  customerId: string;
+  fullName: string;
+  phoneNumber: string;
+  email: string;
+  address: object;
 }
-//이름 변경하셈
-export const usePaymentStore  = create((set)=>({
-  product:null, //객체로저장? 배열로 저장?
-  // setProduct: (state)=>{}
+
+type State = {
+  orderName: string;
+  totalAmount: number;
+  products: Products;
+  customer: Customer;
+}
+type Actions = {
+  setOrderName: (orderName: string) => void;
+  setTotalAmount: (amount: number) => void;
+  setProducts: (products: Products) => void;
+  setCustomer: (customer: Customer) => void;
+  resetState: () => void;
+}
+
+const initialState = {
+  orderName: '',
+  totalAmount: 0,
+  products:[{
+    id: '',
+    name: '',
+    amount: 0,
+    quantity: 0
+  }],
+  customer:{
+    customerId: '',
+    fullName: '',
+    phoneNumber: '',
+    email: '',
+    address:{}
+  }
+}
+export const usePaymentRequestStore  = create<State&Actions>((set)=>({
+  ...initialState,
+  setOrderName: (orderName) => set((state) => ({ ...state, orderName })),
+  setTotalAmount: (amount) => set((state) => ({ ...state, totalAmount: amount })),
+  setProducts: (products) => set((state) => ({ ...state, products })),
+  setCustomer: (customer) => set((state) => ({ ...state, customer })),
+  resetState: () => set(initialState),
 }))
 
