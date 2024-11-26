f4fd4841fd4d57795ce2a596806798936691f7f4
jonghoon <jonghoon7431@gmail.com>
2024-11-26 22:32:33 +0900

    style: 결제 페이지 ui 틀 잡기

---
+++ src/app/(root)/(payment)/payment/components/PaymentHeader.tsx
@@ -0,0 +1,11 @@
+const PaymentHeader = () => {
+  return (
+    <div className="flex justify-between items-center mb-4 md:mt-16">
+      <button className="text-gray-500">←</button>
+      <h1 className="text-lg font-bold">주문/결제</h1>
+      <div></div>
+    </div>
+  );
+};
+
+export default PaymentHeader;
