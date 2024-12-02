bd81bc607e1e4920e7aafb20ad1ca42c28c4f72b
jonghoon <jonghoon7431@gmail.com>
2024-12-02 11:51:26 +0900

    feat: 결제 페이지 헤더 제작 완료
    1. info icon 추가 및 클릭 시 툴팁 토글링
    2. 페이지 방문 시 아이콘 깜빡임(강조 효과)

--- src/app/(root)/(payment)/payment/components/PaymentHeader.tsx
+++ src/app/(root)/(payment)/payment/components/PaymentHeader.tsx
@@ -1,51 +1,104 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 import { BackButton } from '@/components/icons/BackButton';
 import InfoIcon from '@/components/icons/InfoIcon';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
 
 const PaymentHeader = () => {
-  //TODO 새로고침시 컨펌창
   const router = useRouter();
 
-  const WARNING_MESSAGE =
-    '페이지를 벗어날 경우, 입력한 정보가 저장되지 않습니다';
+  const [isTooltipVisible, setTooltipVisible] = useState(false);
+  const [iconHighLight, setIconHighLight] = useState(false);
 
-  const handleBack = () => {
-    //TODO 컨펌창 이♡쁜 알림창으로 바꾸기 ~
-    if (confirm(WARNING_MESSAGE)) {
-      router.back();
-    }
+  const handleTooltipToggle = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    setTooltipVisible((prev) => !prev);
   };
 
-  const handleBeforeUnload = (event: BeforeUnloadEvent) => {
-    event.preventDefault();
-    return (event.returnValue = WARNING_MESSAGE);
-    // window.history.pushState(null, '', window.location.href);
+  // 화면 외부 클릭 시 툴팁 닫기
+  const handleClickOutsideInfoIcon = () => {
+    setTooltipVisible(false);
   };
+
   useEffect(() => {
-    window.addEventListener(
-      'beforeunload',
-      handleBeforeUnload
-      // ,{
-      //   capture: true
-      // }
-    );
-
+    if (isTooltipVisible) {
+      document.addEventListener('click', handleClickOutsideInfoIcon);
+    }
     return () => {
-      window.removeEventListener('beforeunload', handleBeforeUnload);
+      document.removeEventListener('click', handleClickOutsideInfoIcon);
+    };
+  }, [isTooltipVisible]);
+
+  //페이지 첫 방문시 info 아이콘 깜빡임(강조) 효과
+  useEffect(() => {
+    const isVisit = localStorage.getItem('isVisit');
+
+    if (!isVisit) {
+      localStorage.setItem('isVisit', 'true');
+
+      let blinkCount = 0;
+
+      const interValid = setInterval(() => {
+        setIconHighLight((prev) => !prev);
+        blinkCount++;
+
+        if (blinkCount === 10) {
+          clearInterval(interValid);
+        }
+      }, 500);
+
+      return () => {
+        clearInterval(interValid);
+      };
+    }
+    return () => {
+      localStorage.removeItem('isVisit');
     };
   }, []);
 
   return (
     <div className="flex justify-between items-center mb-4 md:mt-16 m-1">
-      <button onClick={handleBack}>
+      {/* 뒤로가기 버튼 */}
+      <button onClick={() => router.back()}>
         <BackButton />
       </button>
+
       <h1 className="text-lg font-semibold">주문/결제</h1>
-      {/* //TODO InfoIcon 호버시 안내 문구 띄우기 */}
-      <InfoIcon />
+
+      {/* Info 아이콘 */}
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <div
+            className="text-white rounded-lg focus:outline-none cursor-pointer"
+            onClick={handleTooltipToggle} // 클릭 시 툴팁 상태 토글
+          >
+            <InfoIcon color={iconHighLight ? '#9C6D2E' : '#959595'} />
+          </div>
+
+          {/* 툴팁 */}
+          {isTooltipVisible && (
+            <div className="absolute top-full transform -translate-x-1/2 mt-3 right-[-9rem]">
+              <div className="relative bg-[#9C6D2E] p-4 w-[274px] text-white text-sm rounded-[8px] shadow-lg select-none ">
+                해당 결제는 가결제입니다. <br />
+                결제 당일 23시-0시 이내 자동 환불됩니다. <br />
+                직접 환불을 원할 시 주문 내역에서 주문 취소 <br />
+                버튼을 통해 환불 가능합니다.
+                {/* 화살표 */}
+                <div
+                  className="absolute top-[-0.4rem] right-[2%] transform -translate-x-1/2 w-0 h-0"
+                  style={{
+                    borderLeft: '8px solid transparent',
+                    borderRight: '8px solid transparent',
+                    borderBottom: '8px solid #9C6D2E'
+                  }}
+                ></div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
