b61524bff74213a77314aa5128fde64b73e2eae0
jonghoon <jonghoon7431@gmail.com>
2024-12-24 17:04:51 +0900

    feat: 쓰로틀링 훅 제작
    버튼용 쓰로틀링 함수.
    적용할 함수, 딜레이를 매개변수로 받습니다.
    적용된 함수, 딜레이 진행  여부, 쓰로틀링 초기화 함수 를 return 합니다.

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,39 @@
+import { useCallback, useRef, useState } from 'react';
+
+type AnyFunction = (...args: any[]) => void;
+type ButtonThrottleProps = {
+  fn: AnyFunction
+  delay: number
+}
+
+function useThrottle ({fn, delay} : ButtonThrottleProps) {
+  const timer = useRef<NodeJS.Timeout | null>(null);
+  const [isDelay, setIsDelay] = useState(false);
+
+  //버튼 쓰로틀링
+  //호출해서 사용할 때 {throttleButtonClick : }로 이름 지정해서 쓰셔요
+  const throttleButtonClick  = useCallback((...args: Parameters<typeof fn>)=>{
+    if(!timer.current){
+      fn(...args);
+      setIsDelay(true)
+
+      timer.current = setTimeout(()=>{
+        timer.current = null;
+        setIsDelay(false)
+      }, delay)
+    }
+  },[fn, delay])
+
+  const resetThrottle = () => {
+    timer.current = null;
+    setIsDelay(false)
+  }
+
+  return {throttleButtonClick , isDelay, resetThrottle}
+}
+export default useThrottle;
+
+
+
+
+
