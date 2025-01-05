6e7394032fcb82cea07a4df5228689e6c0ed83f9
jonghoon <jonghoon7431@gmail.com>
2025-01-06 01:40:28 +0900

    fix: useEffect를 사용한 언마운트시 처리 추가
    reset 함수를 제거하고, 훅 자체에서 언마운트시 초기화하도록 변경했습니다

--- src/hooks/useThrottle.ts
+++ src/hooks/useThrottle.ts
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 type AnyFunction = (...args: any[]) => void;
 type ButtonThrottleProps = {
@@ -15,21 +15,25 @@ function useThrottle ({fn, delay} : ButtonThrottleProps) {
   const throttleButtonClick  = useCallback((...args: Parameters<typeof fn>)=>{
     if(!timer.current){
       fn(...args);
-      setIsDelay(true)
+      setIsDelay(true);
 
       timer.current = setTimeout(()=>{
         timer.current = null;
-        setIsDelay(false)
+        setIsDelay(false);
       }, delay)
     }
   },[fn, delay])
 
-  const resetThrottle = () => {
-    timer.current = null;
-    setIsDelay(false)
-  }
+  useEffect(() => {
+    return () => {
+      if (timer.current) {
+        clearTimeout(timer.current);
+        timer.current = null;
+      }
+    };
+  }, []);
 
-  return {throttleButtonClick , isDelay, resetThrottle}
+  return {throttleButtonClick , isDelay}
 }
 export default useThrottle;
 
