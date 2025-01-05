30f6a6db0489abbdae43d34030a93cf4c38e9d71
jonghoon <jonghoon7431@gmail.com>
2025-01-06 03:06:11 +0900

    feat: ssr로 배송지 불러오기

--- src/app/(root)/my-page/setting/delivery-address/page.tsx
+++ src/app/(root)/my-page/setting/delivery-address/page.tsx
@@ -1,17 +1,58 @@
 // 배송지 관리 페이지
 
+import api from '@/service/service';
+import { AllAddresses } from '@/types/deliveryAddress';
+import { createClient } from '@/utils/supabase/server';
 import AddNewAddressButton from './_components/AddNewAddressButton';
 import NoDeliveryAddress from './_components/NoDeliveryAddress';
 
-const DeliveryAddressManagement = () => {
-  const hasNoAddress = true;
+const getAddress = async () => {
+  const supabase = createClient();
+
+  const { data } = await supabase.auth.getUser();
+  if (!data.user) {
+    return console.error('유저 정보 가져오기 실패');
+  }
+  const userId = data.user.id;
+
+  return await api.address.getAddresses(userId);
+};
+
+const DeliveryAddressManagement = async () => {
+  const allAddresses: AllAddresses = await getAddress();
+  const { defaultAddress, addresses } = allAddresses;
+
+  const hasNoDefaultAddress: boolean = defaultAddress === null;
+  const hasNoAddresses: boolean = hasNoDefaultAddress && addresses === null;
+  const { addressName, receiverName, phoneNumber, baseAddress, detailAddress } =
+    defaultAddress;
   return (
     <>
-      {!hasNoAddress ? (
+      {hasNoAddresses ? (
         <NoDeliveryAddress />
       ) : (
         <div className="max-w-md mx-auto flex flex-col p-4 bg-normal">
-          {/* 내부 요소 들어갈 부분 */}
+          {!hasNoDefaultAddress && (
+            <div>
+              {addressName},{receiverName},{phoneNumber},{baseAddress},
+              {detailAddress}
+            </div>
+          )}
+          {addresses.map((address, index) => {
+            const {
+              addressName,
+              receiverName,
+              phoneNumber,
+              baseAddress,
+              detailAddress
+            } = address;
+            return (
+              <div key={index}>
+                {addressName},{receiverName},{phoneNumber},{baseAddress},
+                {detailAddress}
+              </div>
+            );
+          })}
         </div>
       )}
       <div className="p-4 flex justify-center bg-white shadow-md fixed bottom-0 w-full">
