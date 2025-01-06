dc1dc130ca3b63aeb374effd700bddf32557ba92
jonghoon <jonghoon7431@gmail.com>
2025-01-06 15:27:20 +0900

    fix: fetch cache-control 추가

--- src/service/address.service.ts
+++ src/service/address.service.ts
@@ -6,7 +6,9 @@ class AddressAPI {
   async getAddresses(userId: string){
     if(!userId) throw new Error('유저 ID를 찾을 수 없습니다');
 
-    const response = await fetch(`${ADDRESS_API_URL}?userId=${userId}`);
+    const response = await fetch(`${ADDRESS_API_URL}?userId=${userId}`,{
+      cache: 'no-store',
+    });
     if(!response.ok) throw new Error ('배송지 정보를 가져올 수 없습니다');
 
     return response.json();
