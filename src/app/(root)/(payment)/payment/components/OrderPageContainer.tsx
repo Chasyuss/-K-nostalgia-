d35b88f50c68e7465736378c5419a1c314014803
jonghoon <jonghoon7431@gmail.com>
2025-01-13 23:20:56 +0900

    refactor: 로직 위치 변경

--- src/app/(root)/(payment)/payment/components/OrderPageContainer.tsx
+++ src/app/(root)/(payment)/payment/components/OrderPageContainer.tsx
@@ -1,34 +1,43 @@
-import HeaderWithInfoIcon from '@/components/common/header/_component/HeaderWithInfoIcon';
+'use client';
+
+import { AllAddresses } from '@/types/deliveryAddress';
+import useDeliveryStore from '@/zustand/payment/useDeliveryStore';
+import { usePaymentRequestStore } from '@/zustand/payment/usePaymentStore';
+import { useState } from 'react';
 import DeliveryAddress from './(address)/DeliveryAddress ';
 import CouponInPaymentPage from './CouponInPaymentPage';
 import OrderProducts from './OrderProducts';
 import OrderSummary from './OrderSummary';
 import PaymentMethodSelect from './PaymentMethodSelect';
 
-const OrderPageContainer = () => {
-  //헤더 info 아이콘에 전달할 툴팁 내용
-  const toolTipContentArray = [
-    '해당 결제는 가결제입니다.',
-    '결제 당일 23시-0시 이내 자동 환불됩니다.',
-    '직접 환불을 원할 시',
-    '주문 내역에서 주문 취소 버튼을 통해 환불 가능합니다.'
-  ];
+interface Props {
+  initialAddresses: AllAddresses;
+}
+
+const OrderPageContainer = ({ initialAddresses }: Props) => {
+  const [shippingRequest, setShippingRequest] = useState<string>('');
+  const [shouldStoreDeliveryRequest, setShouldStoreDeliveryRequest] =
+    useState(false);
+
+  const { address } = useDeliveryStore();
+  const { products, resetState } = usePaymentRequestStore();
 
   return (
     <main className="max-w-md mx-auto p-4 bg-normal">
-      <HeaderWithInfoIcon
-        toolTipContentArray={toolTipContentArray}
-        isIncludeIconHighlighting={true}
+      {/* 배송지 */}
+      <DeliveryAddress
+        initialData={initialAddresses}
+        shippingRequest={shippingRequest}
+        shouldStoreDeliveryRequest={shouldStoreDeliveryRequest}
+        setShippingRequest={setShippingRequest}
+        setShouldStoreDeliveryRequest={setShouldStoreDeliveryRequest}
       />
 
-      {/* 배송지 */}
-      <DeliveryAddress />
-
       {/* 주문 상품 */}
-      <OrderProducts />
+      <OrderProducts products={products} resetState={resetState} />
 
       {/* 할인 쿠폰 */}
-      {/****** 추후 작업.(구매 전 쿠폰 적용 작업 이후) */}
+      {/* TODO 추후 작업.(구매 전 쿠폰 적용 작업 이후) */}
       <CouponInPaymentPage />
 
       {/* 결제 수단 선택 */}
