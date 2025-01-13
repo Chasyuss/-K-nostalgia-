76cf6652cb235ca1eca5433ffdb6bc4e00db9f3e
jonghoon <jonghoon7431@gmail.com>
2025-01-13 23:20:13 +0900

    refactor: 컴포넌트 분리 구조 변경

--- src/app/(root)/(payment)/payment/page.tsx
+++ src/app/(root)/(payment)/payment/page.tsx
@@ -1,8 +1,29 @@
+import HeaderWithInfoIcon from '@/components/common/header/_component/HeaderWithInfoIcon';
+import { getAddressesInServerComponent } from '@/hooks/deliveryAddress/getAddresses';
+import { AllAddresses } from '@/types/deliveryAddress';
 import OrderPageContainer from './components/OrderPageContainer';
 
-const Payment = () => {
+const Payment = async () => {
   //TODO 메타 데이터 추가
-  return <OrderPageContainer />;
+  //헤더 info 아이콘에 전달할 툴팁 내용
+  const toolTipContentArray = [
+    '해당 결제는 가결제입니다.',
+    '결제 당일 23시-0시 이내 자동 환불됩니다.',
+    '직접 환불을 원할 시',
+    '주문 내역에서 주문 취소 버튼을 통해 환불 가능합니다.'
+  ];
+
+  const allAddresses: AllAddresses = await getAddressesInServerComponent();
+
+  return (
+    <>
+      <HeaderWithInfoIcon
+        toolTipContentArray={toolTipContentArray}
+        isIncludeIconHighlighting={true}
+      />
+      <OrderPageContainer initialAddresses={allAddresses} />;
+    </>
+  );
 };
 
 export default Payment;
