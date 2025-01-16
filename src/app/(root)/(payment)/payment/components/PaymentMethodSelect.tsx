d6bc998eee5ae5ce52eb39c2a0f7d231549b4a48
jonghoon <jonghoon7431@gmail.com>
2025-01-17 03:02:49 +0900

    fix: pay method 배열 제작, map으로 ui 변경

--- src/app/(root)/(payment)/payment/components/PaymentMethodSelect.tsx
+++ src/app/(root)/(payment)/payment/components/PaymentMethodSelect.tsx
@@ -1,20 +1,28 @@
+'use client';
+
+import { usePaymentRequestStore } from '@/zustand/payment/usePaymentStore';
+
 const PaymentMethodSelect = () => {
+  const paymentMethods = ['토스페이', '카카오페이', '일반결제'];
+  const { payMethod, setPayMethod } = usePaymentRequestStore();
+
   return (
-    <div className="bg-white p-4 rounded shadow mb-4">
-      <h2 className="text-gray-600 font-bold">결제 수단</h2>
-      <div className="mt-2 space-y-2">
-        <label className="flex items-center">
-          <input type="radio" name="payment" className="mr-2" />
-          <span className="text-gray-700">토스페이</span>
-        </label>
-        <label className="flex items-center">
-          <input type="radio" name="payment" className="mr-2" />
-          <span className="text-gray-700">카카오페이</span>
-        </label>
-        <label className="flex items-center">
-          <input type="radio" name="payment" className="mr-2" />
-          <span className="text-gray-700">일반결제</span>
-        </label>
+    <div className="bg-white p-4 flex flex-col gap-2 rounded-[12px] border-2 border-[#E0E0E0] mb-4">
+      <h2 className="text-label-strong text-[18px] font-semibold">결제 수단</h2>
+      <div className="">
+        {paymentMethods.map((method) => (
+          <label key={method} className="flex gap-1 items-center">
+            <input
+              type="radio"
+              name="payment"
+              value={method}
+              checked={payMethod === method}
+              onChange={() => setPayMethod(method)}
+              className="accent-primary-20 w-5"
+            />
+            <span className="text-gray-700">{method}</span>
+          </label>
+        ))}
       </div>
     </div>
   );
