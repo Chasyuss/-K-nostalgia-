6ab793d7bed63333c098736a5910e1e2d01bbf29
jonghoon <jonghoon7431@gmail.com>
2025-01-17 03:06:08 +0900

    chore: 주석 제거

--- src/components/ui/PayButton.tsx
+++ src/components/ui/PayButton.tsx
@@ -27,7 +27,7 @@ type Props = {
   orderNameArr: string[];
   product: ProductProps;
   text: string; //버튼 텍스트 - 버튼 비활성화 및 스타일링에 사용
-  //TODO isCouponApplied: boolean
+  //isCouponApplied: boolean
 };
 
 type setGlobalStateParams = {
@@ -46,29 +46,19 @@ const PayButton = ({ orderNameArr, product, text }: Props) => {
     usePaymentRequestStore(); //zustand
 
   //총액 계산 함수
-  //TODO 쿠폰 할인 적용 작업 끝나면 넘겨받은 금액이 배송비, 쿠폰할인 포함 총 가격인지 재확인 할 것
   const calculateTotalAmount = (product: ProductProps) => {
     return product.reduce((acc, item) => acc + item.amount, 0);
   };
+  const DELIVERY_FEE = 2500;
 
   //payRequest값 저장(zustand)
   const setPayRequestDataInGlobalState = ({
-    orderNameArr,
-    nowUser
+    orderNameArr
   }: setGlobalStateParams) => {
     const totalAmount = calculateTotalAmount(product);
-    setTotalAmount(totalAmount);
-    setOrderName(orderNameArr.join(','));
+    setTotalAmount(totalAmount + DELIVERY_FEE);
+    setOrderName(orderNameArr.join(' '));
     setProducts(product as any);
-
-    // const { id, name, phoneNumber, email, address } = nowUser;
-    // setCustomer({
-    //   customerId: id,
-    //   fullName: name as string,
-    //   phoneNumber: phoneNumber as string,
-    //   email: email as string,
-    //   address: address as object
-    // });
   };
 
   //전역 관리 이후 redirect
@@ -87,17 +77,7 @@ const PayButton = ({ orderNameArr, product, text }: Props) => {
     router.push('/payment');
   };
 
-  // const pathName = usePathname();
   // const [isPaymentOpen, setIsPaymentOpen] = useState<boolean>(false);
-
-  // const date = dayjs(new Date(Date.now())).locale('ko').format('YYMMDD');
-  // const newPaymentId = `${date}-${uuidv4().slice(0, 13)}`; //주문 번호 생성
-
-  // const totalQuantity = product.reduce((acc, item) => acc + item.quantity, 0);
-  // const isCouponApplied: boolean = true; //TODO 쿠폰 적용 여부 더미 (테스트용)
-
-  // const [lastCallTime, setLastCallTime] = useState(0);
-  // const THROTTLE_DELAY = 5000;
 
   // useEffect(() => {
   //   //결제 창 활성화 시 PopStateEvent 제한
@@ -118,113 +98,6 @@ const PayButton = ({ orderNameArr, product, text }: Props) => {
   //     window.removeEventListener('popstate', handlePopstate);
   //   };
   // }, [isPaymentOpen]);
-
-  //결제 요청 함수 with throttling
-  //TODO throttle을 hooks 폴더 내 파일로 분리하는 것 고려
-  // const onPayButtonClickThrottled = useCallback(async () => {
-  //   if (product.length === 0) {
-  //     return toast({
-  //       description: '구매할 상품을 선택 해 주세요'
-  //     });
-  //   }
-
-  //   const now = Date.now();
-
-  //   if (now - lastCallTime >= THROTTLE_DELAY) {
-  //     setLastCallTime(now);
-  //     setIsPaymentOpen(true);
-  //     window.history.pushState(null, '', window.location.href);
-
-  //     toast({
-  //       variant: 'destructive',
-  //       description: '가결제입니다, 당일 자정 전 일괄 환불됩니다'
-  //     });
-  //     setTimeout(() => {
-  //       toast({
-  //         variant: 'destructive',
-  //         description: '즉시 환불은 마이페이지에서 가능합니다'
-  //       });
-  //     }, 1500);
-
-  //     const { name, email, id } = user;
-  //     const requestOrderName = orderNameArr.join(',');
-
-  //     //결제 요청
-  //     const response = await PortOne.requestPayment({
-  //       storeId: process.env.NEXT_PUBLIC_STORE_ID as string,
-  //       channelKey: process.env.NEXT_PUBLIC_INICIS_CHANNEL_KEY,
-  //       paymentId: `${newPaymentId}`,
-  //       orderName: requestOrderName,
-  //       totalAmount,
-  //       currency: 'CURRENCY_KRW',
-  //       payMethod: 'CARD',
-  //       products: product as any,
-  //       redirectUrl:
-  //         process.env.NODE_ENV === 'production'
-  //           ? `https://https://k-nostalgia-one.vercel.app/check-payment?totalQuantity=${totalQuantity}&isCouponApplied=${isCouponApplied}`
-  //           : `http://localhost:3000/check-payment?totalQuantity=${totalQuantity}&isCouponApplied=${isCouponApplied}`,
-  //       appScheme:
-  //         process.env.NODE_ENV === 'production'
-  //           ? `https://https://k-nostalgia-one.vercel.app/check-payment?totalQuantity=${totalQuantity}&isCouponApplied=${isCouponApplied}`
-  //           : `http://localhost:3000/check-payment?totalQuantity=${totalQuantity}&isCouponApplied=${isCouponApplied}`,
-  //       noticeUrls: [
-  //         //webhook url
-  //         `https://k-nostalgia-one.vercel.app/api/payment/webhook`, //실 배포 url
-  //         'https://k-nostalgia-vdpl.vercel.app/api/payment/webhook', //테스트용 배포 url
-  //         'https://7ac2-121-163-241-29.ngrok-free.app/api/payment/webhook' //테스트용 ngrok 서버
-  //       ],
-
-  //       customer: {
-  //         customerId: id,
-  //         email: email as string,
-  //         phoneNumber: '01000000000',
-  //         fullName: name as string
-  //       },
-  //       windowType: {
-  //         pc: 'IFRAME',
-  //         mobile: 'REDIRECTION'
-  //       },
-  //       bypass: {
-  //         inicis_v2: {
-  //           acceptmethod: [`SKIN(#586452)`]
-  //         }
-  //       }
-  //     });
-
-  //     const paymentId = response?.paymentId;
-
-  //     //response.code가 존재 === 결제 실패
-  //     if (response?.code != null) {
-  //       toast({
-  //         variant: 'destructive',
-  //         description: '결제에 실패했습니다 다시 시도해주세요'
-  //       });
-  //       setIsPaymentOpen(false);
-  //       setLastCallTime(0);
-  //       return router.replace(`${pathName}`);
-  //     }
-  //     setIsPaymentOpen(false);
-
-  //     //결제 확인 페이지로 이동(check-payment)
-  //     //paymentId : 내역 조회에 사용
-  //     router.push(
-  //       `/check-payment?paymentId=${paymentId}&totalQuantity=${totalQuantity}&isCouponApplied=${isCouponApplied}`
-  //     );
-  //   } else {
-  //     toast({
-  //       description: '결제 창 요청중입니다'
-  //     });
-  //   }
-  // }, [
-  //   user,
-  //   product,
-  //   orderNameArr,
-  //   totalQuantity,
-  //   totalAmount,
-  //   router,
-  //   pathName,
-  //   lastCallTime
-  // ]);
 
   const ButtonStylesObj: ButtonStylesObj = {
     '바로 구매하기':
